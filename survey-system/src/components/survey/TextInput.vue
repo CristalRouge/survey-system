b2ae71cfd06360141489d21a38446e9577fb1271
Cristal Rouge <qyyao888@gmail.com>
2026-04-16 23:18:05 +0800

    Add survey-system/src/components/survey/TextInput.vue

---
+++ survey-system/src/components/survey/TextInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <textarea
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :placeholder="placeholder"
+    rows="4"
+    class="w-full p-4 border border-gray-300 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+  ></textarea>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: { type: String, default: '' },
+  placeholder: { type: String, default: '请输入...' }
+})
+defineEmits(['update:modelValue'])
+</script>
