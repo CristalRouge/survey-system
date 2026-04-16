f71bd2bb76c09be5129b1dfb68ad266920d164dd
Cristal Rouge <qyyao888@gmail.com>
2026-04-16 23:17:54 +0800

    Add survey-system/src/components/survey/ImageDisplay.vue

---
+++ survey-system/src/components/survey/ImageDisplay.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="src" class="my-4">
+    <img :src="src" :alt="alt" class="max-w-full rounded-lg" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  src: { type: String, default: '' },
+  alt: { type: String, default: 'Image' }
+})
+</script>
