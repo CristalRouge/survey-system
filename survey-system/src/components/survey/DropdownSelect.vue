fb775c14bec82662d8cafb5b7fd4bc2b4d545c8c
Cristal Rouge <qyyao888@gmail.com>
2026-04-16 23:17:52 +0800

    Add survey-system/src/components/survey/DropdownSelect.vue

---
+++ survey-system/src/components/survey/DropdownSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <select
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+    class="w-full p-4 border border-gray-300 rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+  >
+    <option value="" disabled>{{ placeholder }}</option>
+    <option v-for="option in options" :key="option.id" :value="option.id">
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: { type: String, default: '' },
+  options: { type: Array, default: () => [] },
+  placeholder: { type: String, default: '请选择...' }
+})
+defineEmits(['update:modelValue'])
+</script>
