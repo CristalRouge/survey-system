1aff14a846a83ea91777a3ab2c88392cd6a3bce2
Cristal Rouge <qyyao888@gmail.com>
2026-04-16 23:19:57 +0800

    Add survey-backend/src/main/java/com/survey/mapper/SurveyResponseMapper.java

---
+++ survey-backend/src/main/java/com/survey/mapper/SurveyResponseMapper.java
@@ -0,0 +1,9 @@
+package com.survey.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.survey.entity.SurveyResponse;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SurveyResponseMapper extends BaseMapper<SurveyResponse> {
+}
